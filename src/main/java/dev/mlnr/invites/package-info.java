d22902a46e1efdbf87cc392be53e74fc0fa6f753
caneleex <git@mlnr.dev>
2020-07-14 18:08:47 +0200

    Add package-info

---
+++ src/main/java/dev/mlnr/invites/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * This is where the magic happens.
+ *
+ * The package contains 3 classes:
+ * <ul>
+ *     <li>{@link dev.mlnr.invites.InviteData InviteData}
+ *     <br>Used to contain invite's guild id and invite's usage count</li>
+ *
+ *     <li>{@link dev.mlnr.invites.InviteTracking InviteTracking}
+ *     <br>Used to listen to events and cache/uncache invites</li>
+ *
+ *     <li>{@link dev.mlnr.invites.Main}
+ *     <br>Used to build {@link net.dv8tion.jda.api.JDA JDA}</li>
+ * </ul>
+ */
+package dev.mlnr.invites;
