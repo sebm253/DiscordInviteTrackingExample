1e7a48189bc792cca9e0d42d02809ee8067a862f
caneleex <git@mlnr.dev>
2020-07-14 17:51:36 +0200

    Rename WrappedInvite to InviteData

--- src/main/java/dev/mlnr/invites/InviteData.java
+++ src/main/java/dev/mlnr/invites/InviteData.java
@@ -2,12 +2,12 @@ package dev.mlnr.invites;
 
 import net.dv8tion.jda.api.entities.Invite;
 
-public class WrappedInvite // this object is useful to prevent storing jda entities - check https://github.com/DV8FromTheWorld/JDA#entity-lifetimes
+public class InviteData // this object is useful to prevent storing jda entities - check https://github.com/DV8FromTheWorld/JDA#entity-lifetimes
 {
     private final long guildId; // store guild's id
     private int uses; // store invite's usage count
 
-    public WrappedInvite(final Invite invite)
+    public InviteData(final Invite invite)
     {
         this.guildId = invite.getGuild().getIdLong();
         this.uses = invite.getUses();
--- src/main/java/dev/mlnr/invites/InviteTracking.java
+++ src/main/java/dev/mlnr/invites/InviteTracking.java
@@ -17,14 +17,14 @@ import java.util.stream.Collectors;
 
 public class InviteTracking extends ListenerAdapter
 {
-    private final Map<String, WrappedInvite> inviteCache = new HashMap<>(); // initialize a Map for invites; key - a String, invite's code; value - WrappedInvite object to prevent storing jda entities
+    private final Map<String, InviteData> inviteCache = new HashMap<>(); // initialize a Map for invites; key - a String, invite's code; value - InviteData object to prevent storing jda entities
 
     @Override
     public void onGuildInviteCreate(@NotNull final GuildInviteCreateEvent event) // gets fired when an invite is created, lets cache it
     {
         final var code = event.getCode(); // get invite's code
-        final var wrapped = new WrappedInvite(event.getInvite()); // create a WrappedInvite object for the invite
-        inviteCache.put(code, wrapped); // put code as a key and WrappedInvite object as a value into the map; cache
+        final var inviteData = new InviteData(event.getInvite()); // create a InviteData object for the invite
+        inviteCache.put(code, inviteData); // put code as a key and InviteData object as a value into the map; cache
     }
 
     @Override
@@ -52,7 +52,7 @@ public class InviteTracking extends ListenerAdapter
                                                                         .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)); // collect into a new map
             for (final var retrievedInvite : retrievedInvites) // iterate through retrieved invites
             {
-                final var cachedInvite = storedInvitesForThisGuild.get(retrievedInvite.getCode()); // get WrappedInvite object for this invite
+                final var cachedInvite = storedInvitesForThisGuild.get(retrievedInvite.getCode()); // get InviteData object for this invite
                 if (retrievedInvite.getUses() > cachedInvite.getUses()) // check if retrieved invite's usage count is bigger than the cached one's
                 {
                     cachedInvite.incrementUses(); // increment cached invite's usage count
@@ -91,6 +91,6 @@ public class InviteTracking extends ListenerAdapter
 
         if (selfMember.hasPermission(Permission.MANAGE_SERVER)) // check if your bot has MANAGE_SERVER permission to retrieve the invites
             guild.retrieveInvites().queue(retrievedInvites ->
-                    retrievedInvites.forEach(retrievedInvite -> inviteCache.put(retrievedInvite.getCode(), new WrappedInvite(retrievedInvite)))); // iterate through invites and store them
+                    retrievedInvites.forEach(retrievedInvite -> inviteCache.put(retrievedInvite.getCode(), new InviteData(retrievedInvite)))); // iterate through invites and store them
     }
 }
