fc8c6263eb8ff21dfc45656ef5f55d6e82b81cd9
caneleex <git@mlnr.dev>
2020-07-15 15:05:04 +0200

    Make InviteData an inner class

--- src/main/java/dev/mlnr/invites/package-info.java
+++ src/main/java/dev/mlnr/invites/package-info.java
@@ -3,11 +3,11 @@
  *
  * The package contains 3 classes:
  * <ul>
- *     <li>{@link dev.mlnr.invites.InviteData InviteData}
- *     <br>Used to contain invite's guild id and invite's usage count</li>
- *
  *     <li>{@link dev.mlnr.invites.InviteTracking InviteTracking}
  *     <br>Used to listen to events and cache/uncache invites</li>
+ *
+ *     <li>{@link dev.mlnr.invites.InviteTracking.InviteData InviteTracking.InviteData}
+ *     <br>Used to contain invite's guild id and invite's usage count</li>
  *
  *     <li>{@link dev.mlnr.invites.Main}
  *     <br>Used to build {@link net.dv8tion.jda.api.JDA JDA}</li>
